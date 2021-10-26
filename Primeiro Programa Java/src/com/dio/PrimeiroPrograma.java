30eacb2a7c3b25e3051430e11c46eb97095df575
LUCIO PASSOS PATROCINIO <79651078+LPATROCINIO1969@users.noreply.github.com>
2021-10-26 11:33:26 -0300

    Add files via upload

---
+++ Primeiro Programa Java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 2;
+		
+		int b = 3;
+		
+		System.out.println("Hello, Word! "+(a+b));
+		Gato gato = new Gato("shana","amarela",8);
+		System.out.println(gato);
+	}
+
+}
